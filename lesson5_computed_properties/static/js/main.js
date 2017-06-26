716773742eaa2bbcceb2414eb4b2b71e22a10110
Edwin Marte Torres <emarte@intellisys.com.do>
2017-06-26 16:53:45 -0400

    Lesson 6 - Filters

--- lesson5_computed_properties/static/js/main.js
+++ lesson5_computed_properties/static/js/main.js
@@ -2,7 +2,25 @@
 const vm = new Vue({
     el: 'main',
     data: {
-
+        message: 'Hello World :D !',
+        newTask: null,
+        tasks: [
+            {
+                title: 'Learn Vue.js',
+                priority: true,
+                age: 23
+            },
+            {
+                title: 'Learn ES6',
+                priority: false,
+                age: 135
+            },
+            {
+                title: 'Publish something new everyday',
+                priority: true,
+                age: 378
+            }
+        ]
     },
     filters: {
         capitalize: function (value) {
@@ -16,6 +34,25 @@ const vm = new Vue({
         }
     },
     methods: {
-
+        addTask() {
+            // this, makes reference to the Vue instance
+            this.tasks.unshift({
+                title: this.newTask,
+                priority: true,
+                age: 0,
+            });
+            this.newTask = null;
+        }
+    },
+    computed: {
+        reversedMessage() {
+            return this.message.split('').reverse().join('')
+        },
+        prioritizedTasks() {
+            return this.tasks.filter((task) => task.priority)
+        },
+        tasksByAge() {
+            return this.tasks.sort((a, b) => b.age - a.age)
+        }
     }
 });
---
+++ lesson6_filters/static/js/main.js
@@ -0,0 +1,49 @@
+Vue.filter('backwards', (value) => value.split('').reverse().join(''));
+
+// Vue.js
+const vm = new Vue({
+    el: 'main',
+    data: {
+        search: '',
+        minimum: 5,
+        games: [
+            {
+                title: 'Battlefield 1',
+                genre: 'FPS',
+                ranking: 9
+            },
+            {
+                title: 'Civilization VI',
+                genre: 'Strategy',
+                ranking: 10
+            },
+            {
+                title: 'Resident Evil 7',
+                genre: 'Survival Horror',
+                ranking: 7
+            },
+        ]
+    },
+    filters: {
+        capitalize: function (value) {
+            if (!value) return '';
+            value = value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        uppercase: function (value) {
+            if (!value) return '';
+            return value.toUpperCase();
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+        bestGames() {
+            return this.games.filter((game) => game.ranking >= this.minimum)
+        },
+        searchGame() {
+            return this.games.filter((game) => game.title.includes(this.search));
+        }
+    }
+});
